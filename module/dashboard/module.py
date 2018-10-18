88ef1fe1c414999586a5149b35080c1f7d1cf885
Nikolay Istomin <raduetsya@gmail.com>
2018-10-18 13:32:46 +0700

    Add new module ck-web:module:dashboard. Support command `ck display dashboard` which opens web-browser with data from ck-web:wfe:dashboard

---
+++ module/dashboard/module.py
@@ -0,0 +1,72 @@
+#
+# Collective Knowledge (CK web front-end)
+#
+# See CK LICENSE.txt for licensing details
+# See CK COPYRIGHT.txt for copyright details
+#
+# Developer: cTuning foundation
+#
+
+cfg={}  # Will be updated by CK (meta description of this module)
+work={} # Will be updated by CK (temporal data)
+ck=None # Will be updated by CK (initialized CK kernel) 
+
+# Local settings
+
+import os
+
+fdata_name='ck_top_data'
+fmodule_name='ck_top_module'
+frepo_name='ck_top_repo'
+
+form_name='ck_top_form'
+onchange='document.'+form_name+'.submit();'
+
+##############################################################################
+# Initialize module
+
+def init(i):
+    """
+
+    Input:  {}
+
+    Output: {
+              return       - return code =  0, if successful
+                                         >  0, if error
+              (error)      - error text if return > 0
+            }
+
+    """
+    return {'return':0}
+
+
+##############################################################################
+# display webpage with interactive dashboard widget
+
+def display(i):
+    """
+    Input:  {
+              (host)        - Internal web server host
+              (port)        - Internal web server port
+
+              (wfe_host)    - External web server host
+              (wfe_port)    - External web server port
+
+              (extra_url)   - extra URL
+            }
+
+    Output: {
+              return       - return code =  0, if successful
+                                         >  0, if error
+              (error)      - error text if return > 0
+            }
+
+    """
+
+    i['action']='start'
+    i['module_uoa']='web'
+    i['browser']='yes'
+    i['template']='dashboard'
+    i['cid']=''
+
+    return ck.access(i)
